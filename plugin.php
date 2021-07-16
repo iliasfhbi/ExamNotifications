6fdadf35122fa87321facaeea4eb16a4e23e06d6
Matthias Stock <matthias.stock@fh-bielefeld.de>
2021-07-16 08:14:08 +0000

    Update plugin.php

--- plugin.php
+++ plugin.php
@@ -7,4 +7,4 @@ $ilias_min_version = "5.4.0";
 $ilias_max_version = "5.4.999";
 
 $responsible = "Sebastian Otte";
-$responsible_mail = "sebastian.otte@fh-bielefeld.de";
+$responsible_mail = "ilias@fh-bielefeld.de";
